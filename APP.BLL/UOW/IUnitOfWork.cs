fe5e306b207dc5e7c5c3920341c939072988d04d
zbububaiz <phongbu257@gmail.com>
2025-03-20 14:35:28 +0700

    feat: Comment API and adjust Blog API

--- APP.BLL/UOW/IUnitOfWork.cs
+++ APP.BLL/UOW/IUnitOfWork.cs
@@ -12,6 +12,7 @@ namespace APP.BLL.UOW
         IExpiredTokenRepository ExpiredTokens { get; }
         IBlogRepository Blogs { get; }
         ISkinTherapistRepository SkinTherapists { get; }
+        ICommentRepository Comments { get; }
         Task<int> SaveAsync();
         Task<int> SaveWithTransactionAsync();
         Task<int> SaveWithTransactionAsync(Func<Task> operation);
