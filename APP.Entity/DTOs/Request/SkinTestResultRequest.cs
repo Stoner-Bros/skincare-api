4f7696b517613c84d2198224fcbf8d76ca800d77
zbububaiz <phongbu257@gmail.com>
2025-03-26 22:43:41 +0700

    update feat: SkinTestResult API

--- APP.Entity/DTOs/Request/SkinTestResultRequest.cs
+++ APP.Entity/DTOs/Request/SkinTestResultRequest.cs
@@ -4,8 +4,6 @@ namespace APP.Entity.DTOs.Request
 {
     public class SkinTestResultRequest
     {
-        public int? CustomerId { get; set; }
-        public int? GuestId { get; set; }
         public string Result { get; set; } = null!;
     }
 }
--- APP.Entity/DTOs/Response/SkinTestResultResponse.cs
+++ APP.Entity/DTOs/Response/SkinTestResultResponse.cs
@@ -5,8 +5,7 @@ namespace APP.Entity.DTOs.Response
     public class SkinTestResultResponse
     {
         public int ResultId { get; set; }
-        public int? CustomerId { get; set; }
-        public int? GuestId { get; set; }
+        public string Email { get; set; } = null!;
         public int SkinTestAnswerId { get; set; }
         public string Result { get; set; } = null!;
         public DateTime CreatedAt { get; set; }
